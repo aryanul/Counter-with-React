faecf4ad6e0f8ceb38f0d2a7ba32cf56b3a9c1c4
Khandakar Aryanul Haque <aryanul2004@gmail.com>
2024-05-17 03:48:38 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  let [counter , setCounter] = useState(1)
+  const addValue = () =>{
+    setCounter(counter + 1)
+  }
+  const decValue =() =>{
+    counter >= 0
+    counter = counter -1
+    
+    setCounter(counter)
+  }
+  // if (counter<0) {
+  //   alert("This counter is not allowed go less than zero !")
+    
+  // }
+  return (
+    <>
+      <h1>Counter: {counter}</h1>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <br />
+      <button onClick={decValue}>Decrease Value</button>
+      
+    </>
+  )
+}
+
+export default App
